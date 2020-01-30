f081bc9c3b637604d7932256d477b90c51a22cc7
Mateusz Nowakowski <manowakowski@getresponse.com>
2020-01-30 21:54:24 +0100

    test

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -3,8 +3,17 @@ require 'amixer2019'
 class AnnouncementsController < ApplicationController
   def create
     amixer = AMixer2019.new
-    amixer.fade '0%'
+
+    60.downto(10) do |i|
+      amixer.volume = "#{i}%"
+      sleep 0.02
+    end
+
     system('./../annc.sh')
-    amixer.fade '60%'
+
+    10.upto(60) do |i|
+      amixer.volume = "#{i}%"
+      sleep 0.02
+    end
   end
 end
