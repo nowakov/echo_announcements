861c637608d285a1337aa9a816b4a1b08bbb2aa2
Mateusz Nowakowski <manowakowski@getresponse.com>
2020-02-06 20:17:45 +0100

    test

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -2,14 +2,14 @@ class AnnouncementsController < ApplicationController
   def create
     50.times do
       `/usr/bin/amixer -q set Master 1%-`
-      sleep 0.05
+      sleep 0.02
     end
 
     `/usr/bin/omxplayer -o local /home/pi/echo/start-#{params[:start]}.mp3`
 
     50.times do
       `/usr/bin/amixer -q set Master 1%+`
-      sleep 0.05
+      sleep 0.02
     end
 
     redirect_to root_path
